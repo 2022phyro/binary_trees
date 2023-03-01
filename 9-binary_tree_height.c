4eeabe735d9185da6a6cc160b2e5d5eb4128b0cd
2022phyro <phyrokelstein2@gmail.com>
2023-03-01 20:11:06 +0100

    no 17 and 18

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,11 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
 	size_t l, r;
 
-	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (l > r ? l : r);
+	if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (l > r ? l : r);
+	}
+	return (0);
 }
