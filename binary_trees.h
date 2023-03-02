bad552339f9867580949f84d0ad6797f238691cc
2022phyro <phyrokelstein2@gmail.com>
2023-03-02 21:53:57 +0100

    Searching a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -63,5 +63,6 @@ bst_t *bst_insert(bst_t **tree, int value);
 binary_tree_t *insert_right(binary_tree_t *parent, int value);
 binary_tree_t *insert_left(binary_tree_t *parent, int value);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 #endif /*BS_H*/
 
